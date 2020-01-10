432f68317755b8db3d4c692827611775ea50d314
diogo <diogorodriguesmatos@gmail.com>
2020-01-10 16:18:53 +0000

    buggg fix

--- Trabalho 3/actions/users/edit_own_account.php
+++ Trabalho 3/actions/users/edit_own_account.php
@@ -3,16 +3,32 @@
 include_once('../../config/init.php');
 include_once($BASE_DIR .'database/users.php');  
 
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes  ($data);
+  $data = htmlspecialchars($data);
+  $data = strip_tags($data);
+  return $data;
+}
+
 
 $id=$_SESSION['id'];
 $user_p = $_POST['login'];
 $email_p = $_POST['email'];
 
 $userdat = getuser($id);
-$row = pg_fetch_row($userdat, 0);
-$user = $row[1];
-$email = $row[3];
-$saldo = $row[4];
+$user = $userdat['username'];
+$email = $userdat['email'];
+$saldo = $userdat['saldo'];
+
+$email = test_input($email);
+ // check if e-mail address is well-formed
+ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $_SESSION['error_messages'][] = "Invalid entity email format";
+      $_SESSION['form_values']=$_POST;
+      //die(header('Location: ' . $_SERVER['HTTP_REFERER']));
+    }
+
 
 if(!empty($user_p) or is_numeric($user_p)){
 	$user=$user_p;
@@ -21,22 +37,27 @@ if(!empty($email_p)){
 	$email=$email_p;
 }
 
+
+echo $email;
 $tr = edituser($id,$user,$email,$saldo);
 
 	if($tr==-1){
-			$_SESSION['ea_u']=1;
-			$_SESSION['ea_e']=0;
-			header("Location:  $BASE_URL" . '/pages/users/account_management.php');
-		}
-		elseif ($tr==-2){
-			$_SESSION['ea_u']=1;
-			$_SESSION['ea_e']=0;
-			header("Location:  $BASE_URL" . '/pages/users/account_management.php');
-		}	
-		elseif ($tr==-3){
-			$_SESSION['ea_u']=1;
-			$_SESSION['ea_e']=0;
-			header("Location:  $BASE_URL" . '/pages/users/account_management.php');
-		}	
+		$_SESSION['error_messages'][] = 'Username is already in use';
+	    $_SESSION['form_values'] = $_POST;
+		header("Location:  $BASE_URL" . '/pages/users/account_management.php');
+	}
+	elseif ($tr==-2){
+	   $_SESSION['error_messages'][] = 'Email is already in use';
+	   $_SESSION['form_values'] = $_POST;
+	   header("Location:  $BASE_URL" . '/pages/users/account_management.php');
+	}	
+	elseif ($tr==-3){
+		 $_SESSION['error_messages'][] = 'Username and Email is already in use';
+	     $_SESSION['form_values'] = $_POST;
+		  header("Location:  $BASE_URL" . '/pages/users/account_management.php');
+	}	
+else{	
 header("Location:  $BASE_URL" . '/pages/users/account_management.php');
+}
+
 ?>
